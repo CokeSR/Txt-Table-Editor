f596d6677648b834a70aedb55271be1adb7a2f11
liangGao-Master <cokeserver@qq.com>
2025-02-17 17:14:26 +0800

    Add language package

---
+++ utils/language.py
@@ -0,0 +1,19 @@
+import sys
+sys.dont_write_bytecode = True
+
+EXE_TITLE = 'TXT 表格编辑器 | Copyright@cokeserver@qq.com'
+
+EXE_BUTTON_UPLOAD = '上传 TXT 文件'
+EXE_BUTTON_SAVE   = '保存 TXT 文件'
+
+EXE_INPUT_LABEL   = '搜索:'
+EXE_INPUT_PLACEHOLDER = '输入关键字...'
+
+EXE_OPEN_FILE_STATUS_COMMON = '未打开文件'
+EXE_OPEN_FILE_STATUS_ACTIVE = '当前文件: %s | 数值修改后会变为绿色；修改结束后请保存该文件以生效'
+
+EXE_SAVE_FILE = '将文件另存为'
+EXE_SAVE_FILE_SUCC = '保存成功'
+EXE_SAVE_FILE_FAIL = '保存失败: %s'
+EXE_SAVE_FILE_FAIL_MSG = '请检查程序日志以排除问题'
+EXE_SAVE_FILE_PATH = '文件已保存至: %s'
